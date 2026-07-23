d0287a0cbe8edeeb2ec5f0247fb8988ef25e027d
2023030575-dot <2023030575@upsin.edu.mx>
2026-07-23 11:10:10 -0700

    PWA

--- src/main.ts
+++ src/main.ts
@@ -31,6 +31,8 @@ import {
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { isDevMode } from '@angular/core';
+import { provideServiceWorker } from '@angular/service-worker';
 
 const firebaseConfig = {
   projectId: 'upsin9-2-c2a6d',
@@ -73,7 +75,10 @@ bootstrapApplication(AppComponent, {
 
     provideFirestore(
       () => getFirestore()
-    )
+    ), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })
   ]
 }).catch((error: unknown) => {
   console.error(
